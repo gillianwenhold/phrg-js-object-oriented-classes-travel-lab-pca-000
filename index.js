f3953c0c74fbf04046baac2018b1fcc5198906e5
Gillian Wenhold <gwenhold@mac.com>
2019-08-10 13:38:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
 
+  yearsExperienceFromBeginningOf(year) {
+    return year - this.startDate.getFullYear();
+  }
+
+}
+
+class Route {
+  constructor(beginningLocation, endLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endLocation = endLocation;
+  }
+
+  blocksTravelled() {
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    let total = 0
+    const beginning = eastWest.indexOf(this.beginningLocation.horizontal)
+    const end = eastWest.indexOf(this.endLocation.horizontal)
+    if (beginning !== end) {
+      total += end - beginning;
+    }
+    total += (this.endLocation.vertical - this.beginningLocation.vertical);
+    return total;
+  }
+
+  estimatedTime(peak = false) {
+    const travelled = this.blocksTravelled()
+    if (peak === false) {
+      return travelled / 3;
+    } else {
+      return travelled / 2;
+    }
+  }
+}
